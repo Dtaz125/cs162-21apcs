f7b40ab8330b263a73270b68d8a16182d717f500
Trần Kim Phát <88208222+phatalways-sleeping@users.noreply.github.com>
2022-03-21 16:36:17 +0700

    Update Class.h

--- CS162_Project/Class.h
+++ CS162_Project/Class.h
@@ -1,27 +1,44 @@
 #pragma once
-#include "student.h"
-#include "linked_list.h"
 #include "Node.h"
-#include "iterator.h"
+#include "Student.h"
 struct Class{
     string name;
-    Linked_List<Student> student_list;
+    Node<Student>* student_list;
     int member;
-    Class (){}
-    Class (string __name, int __member){
-        name = __name;
-        member = __member;
-    }
 };
 
-bool Add_Student(Class &c, Student new_student);
-bool set_name(Class &c, string name);
-
-bool set_name(Class &c, string name){
-    c.name = name;
+bool add_student(const Student& student, Class& _class) {
+    if (contains_student(student, _class)) return false;
+    Node<Student>* newNode = new Node<Student>{ student, nullptr };
+    newNode->next = _class.student_list;
+    _class.student_list = newNode;
     return true;
 }
-bool Add_Student(Class &c, Student new_student){
-    return c.student_list.insert(new_student);
+
+bool contains_student(const Student& student, const Class& _class) {
+    Node<Student>* curr = _class.student_list;
+    while (curr && curr->data != student)
+        curr = curr->next;
+    return curr != nullptr;
 }
 
+bool remove_student(const Student& student, Class& _class) {
+    Node<Student>* curr = _class.student_list, * prev = nullptr;
+    while (curr && curr->data != student) {
+        prev = curr;
+        curr = curr->next;
+    }
+    if (!curr) return false;
+    if (!prev) _class.student_list = _class.student_list->next;
+    else prev->next = curr->next;
+    delete curr;
+    return true;
+}
+
+bool operator == (const Class& class_1, const Class& class_2) {
+    return class_1.name == class_2.name;
+}
+
+bool operator != (const Class& class_1, const Class& class_2) {
+    return class_1.name != class_2.name;
+}
