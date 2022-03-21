f4ae4b053e50a4a21ae1149bb3920af9ed3dd85a
Trần Kim Phát <88208222+phatalways-sleeping@users.noreply.github.com>
2022-03-21 16:40:24 +0700

    Update Student.h

--- CS162_Project/Student.h
+++ CS162_Project/Student.h
@@ -1,26 +1,40 @@
 #pragma once
 #ifndef STUDENT_H
 #define STUDENT_H
-#include"Linked_List.h"
 #include"Account.h"
 #include"Course.h"
 #include"Class.h"
-
+#include"Node.h"
 class Student : public Account {
 private:
 	string studentid;
 	string no;
 	float gpa;
 	Class student_class;
-	Linked_List<Course> list_of_courses;
+	Node<Course>* list_of_courses;
 public:
 	Student() : Account() {}
 	Student(string useracc, string pass, const User& info, string id, string NO) : Account(useracc, pass, info), studentid(id), no(NO) {}
 	const string& getStudentID() { return studentid; }
 	const string& getNO() { return no; }
+	const Class& getClass() { return student_class; }
 	float getGPA() { return gpa; }
 	void setID(const string& id) { studentid = id; }
 	void setNO(const string& NO) { no = NO; }
+	void calculateGPA() {
+
+	}
+	void attend_course(const Course& course) {
+		list_of_courses = new Node<Course>{ course, list_of_courses };
+	}
+	void empty_course() {
+		Node<Course>* curr = nullptr;
+		while (list_of_courses) {
+			curr = list_of_courses;
+			list_of_courses = list_of_courses->next;
+			delete curr;
+		}
+	}
 	friend bool operator == (const Student& student_1, const Student& student_2) {
 		return student_1.studentid == student_2.studentid;
 	}
