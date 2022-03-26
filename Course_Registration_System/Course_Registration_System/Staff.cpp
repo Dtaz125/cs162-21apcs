03f94df15a366c27ebb52022170245696bd2a766
Phát Trần <trankim147@gmail.com>
2022-03-26 07:31:02 +0700

    Fix bugs in Staff.h

--- Course_Registration_System/Course_Registration_System/Staff.cpp
+++ Course_Registration_System/Course_Registration_System/Staff.cpp
@@ -1,18 +1,37 @@
 #include"Staff.h"
 
-Staff::Staff() : Account() {
+Staff::Staff() {
 	//empty
 }
-Staff::Staff(string useracc, string pass, const User& info) : Account(useracc, pass, info) {
+Staff::Staff(string useracc, string pass, const User& info): username(useracc), password(pass), user_info(info){
 	//empty
 }
-Staff::Staff(const Staff& staff) {
-	Account::Account(staff);
+Staff::Staff(const Staff& staff) : username(staff.username), password(staff.password), user_info(staff.user_info) {
+	//empty
 }
-bool Staff::operator == (const Staff& other) {
-
+const string& Staff::getUsername() {
+	return username;
 }
-bool Staff::operator != (const Staff& other) {
+const string& Staff::getPassword() {
+	return password;
+}
+const User& Staff::getInfo() {
+	return user_info;
+}
+void Staff::setUsername(string useracc) {
+	username = useracc;
+}
+void Staff::setPassword(string pass) {
+	password = pass;
+}
+void Staff::setInfo(const User& info) {
+	user_info = info;
+}
+bool operator == (const Staff& first, const Staff& second) {
+	return first.user_info.social_id == second.user_info.social_id;
+}
 
+bool operator != (const Staff& first, const Staff& second) {
+	return first.user_info.social_id != second.user_info.social_id;
 }
 
--- Course_Registration_System/Course_Registration_System/Staff.h
+++ Course_Registration_System/Course_Registration_System/Staff.h
@@ -2,14 +2,22 @@
 #ifndef STAFF_H
 #define STAFF_H
 #include"Account.h"
-class Staff : public Account {
+class Staff {
 private:
-
+	string username;
+	string password;
+	User user_info;
 public:
 	Staff();
 	Staff(string useracc, string pass, const User& info);
 	Staff(const Staff& staff);
-	bool operator == (const Staff& other);
-	bool operator != (const Staff& other);
+	const string& getUsername();
+	const string& getPassword();
+	const User& getInfo();
+	void setUsername(string useracc);
+	void setPassword(string pass);
+	void setInfo(const User& info);
+	friend bool operator == (const Staff& first, const Staff& second);
+	friend bool operator != (const Staff& first, const Staff& second);
 };
 #endif // !STAFF_H
