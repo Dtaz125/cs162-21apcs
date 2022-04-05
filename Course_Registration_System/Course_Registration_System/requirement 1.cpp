571e29e946a932859d80f47e8571586c2e1a2186
Dtaz125 <93046992+Dtaz125@users.noreply.github.com>
2022-04-05 21:04:23 +0700

    update

--- Course_Registration_System/Course_Registration_System/requirement 1.cpp
+++ Course_Registration_System/Course_Registration_System/requirement 1.cpp
@@ -7,15 +7,7 @@
 #include <vector>
 using namespace std;
 
-int convertStringToInt(string _string) {
-	int x;
-	stringstream temp(_string);
-	temp >> x;
-
-	return x;
-}
-
-void inputToVector(Class *classname, string _inputfile) {
+void inputToVector(Class*& classname, const string _inputfile) {
 	string inputFile = _inputfile;
 
 	vector<vector<string>> content;
@@ -34,8 +26,6 @@ void inputToVector(Class *classname, string _inputfile) {
 			content.push_back(row);
 		}
 	}
-	else
-		cout << "Could not open the file." << endl;
 
 
 	for (int i = 0; i < content.size(); i++) {							//loops for every line
@@ -50,9 +40,9 @@ void inputToVector(Class *classname, string _inputfile) {
 			new_user.gender = 0;
 		}
 		else new_user.gender = 1;
-		new_user.birth.day = convertStringToInt(content[i][5]);			//this is where convert to string function is needed. because day month year members are all int datatype
-		new_user.birth.month = convertStringToInt(content[i][6]);
-		new_user.birth.year = convertStringToInt(content[i][7]);
+		new_user.birth.day = stoi(content[i][5]);			//this is where convert to string function is needed. because day month year members are all int datatype
+		new_user.birth.month = stoi(content[i][6]);
+		new_user.birth.year = stoi(content[i][7]);
 
 		new_stu.setInfo(new_user);
 		new_stu.setClass(classname);									//this should set the class of this new student
@@ -79,7 +69,7 @@ int main() {
 	Class class20clc11(class20clc11string, 4);
 	Class class20vp(class20vpstring, 4);
 	
-	Class* class20apcs1Pointer = &class20clc11;				//i have to create a pointer, in order to use the function void setClass(Class* _class) in Student Class
+	Class* class20apcs1Pointer = &class20apcs1;				//i have to create a pointer, in order to use the function void setClass(Class* _class) in Student Class
 	inputToVector(class20apcs1Pointer, "class20apcs1 student information.csv");
 
 
