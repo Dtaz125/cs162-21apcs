cfc9fd963984b25f7ba4856c11e8fa3a1ccf2d93
nhuvinhpro123 <68021529+nhuvinhpro123@users.noreply.github.com>
2022-04-12 10:35:48 +0700

    Update StaffProfile

--- Course_Registration_System/Course_Registration_System/staffProfile.h
+++ Course_Registration_System/Course_Registration_System/staffProfile.h
@@ -1,11 +1,9 @@
 #pragma once
 #include <iostream>
-#include"Initialize.h"
-#include"other.h"
 using namespace std;
 
-//const int baseX = 30;
-//const string blank = "                                                     ";
+///const int baseX = 30;
+///const string blank = "                                                     ";
 #include "Staff.h"
 /*struct Staff{
     string name = "Vinh";
@@ -14,21 +12,21 @@ using namespace std;
     string socialID = "192108211";
 };*/
 
-string getGender(Staff st) {
-    if (st.user_info.gender == 0) return "Male";
+string getGender(Staff st){
+    if (st.getInfo().gender == 0) return "Male";
     return "Female";
 }
 
-void changeName(Staff& st) {
+void changeName(Staff &st){
     string new_name;
-    drawText(baseX, 4, "1. Full name: " + blank);
-    drawText(baseX, 4, "1. Full name: ");
+    drawText(baseX, baseY + 0, "1. Full name: " + blank);
+    drawText(baseX, baseY + 0, "1. Full name: ");
     cin.ignore();
     getline(cin, new_name);
-    User tmp = st.user_info;
+    User tmp = st.getInfo();
     int pos = -1;
     for (int i = new_name.size() - 1; i >= 0; i--) {
-        if (new_name[i] == ' ') {
+        if (new_name[i] == ' '){
             pos = i;
             break;
         }
@@ -40,68 +38,66 @@ void changeName(Staff& st) {
     for (int i = pos + 1; i < new_name.size(); i++)
         tmp.lastname += new_name[i];
 
-    st.user_info = tmp;
+    st.setInfo(tmp);
 }
 
 void changeBirthday(Staff& st) {
-    drawText(baseX, 5, "2. Birthday: " + blank);
-    drawText(baseX, 5, "2. Birthday: ");
+    drawText(baseX, baseY + 1, "2. Birthday: " + blank);
+    drawText(baseX, baseY + 1, "2. Birthday: ");
     cin.ignore();
-    User info = st.user_info;
+    User info = st.getInfo();
     string birth;
     getline(cin, birth);
     if (!validDate(birth, info.birth)) return;
-    st.user_info = info;
+    st.setInfo(info);
 }
-void changeGender(Staff& st) {
+void changeGender(Staff &st){
     string new_gen;
-    drawText(baseX, 6, "3. Gender(Male/Female): " + blank);
-    drawText(baseX, 6, "3. Gender(Male/Female): ");
+    drawText(baseX, baseY + 2, "3. Gender(Male/Female): " + blank);
+    drawText(baseX, baseY + 2, "3. Gender(Male/Female): ");
     cin >> new_gen;
-    User tmp = st.user_info;
+    User tmp = st.getInfo();
     if (new_gen == "Male") tmp.gender = 0;
     else tmp.gender = 1;
-    st.user_info = tmp;
+    st.setInfo(tmp);
 }
-void changeSocialID(Staff& st) {
+void changeSocialID(Staff &st){
     string new_ID;
-    drawText(baseX, 7, "4. Social ID: " + blank);
-    drawText(baseX, 7, "4. Social ID: ");
+    drawText(baseX, baseY + 3, "4. Social ID: " + blank);
+    drawText(baseX, baseY + 3, "4. Social ID: ");
     cin >> new_ID;
-    User tmp = st.user_info;
+    User tmp = st.getInfo();
     tmp.social_id = new_ID;
-    st.user_info = tmp;
-}
-void viewClass(Staff st) {
-
-}
-void viewGPA(Staff st) {}
-void viewListofCourse(Staff st) {
-
+    st.setInfo(tmp);
 }
 
-void drawStaffProfile(Staff& st)
+
+void drawStaffProfile(Staff &st)
 {
+    int baseY = 4;
     system("cls");
     int id;
-    do {
+    do{
         drawText(posCenter("Staff Profile: "), 1, "Staff Profile: ");
-        drawText(baseX, 4, "1. Full name: "); cout << st.user_info.firstname << " " << st.user_info.lastname;
-        drawText(baseX, 5, "2. Birthday: "); cout << st.user_info.birth.day << "/" << st.user_info.birth.month << "/" << st.user_info.birth.year;
-        drawText(baseX, 6, "3. Gender(Male/Female): "); cout << getGender(st);
-        drawText(baseX, 7, "4. Social ID: "); cout << st.user_info.social_id;
-        drawText(baseX, 8, "5. Class: ");
-        drawText(baseX, 9, "6. List of course : ");
-        drawText(baseX, 10, "7. Exit");
+        drawText(baseX, baseY + 0, "1. Full name: "); cout << st.getInfo().firstname << " " << st.getInfo().lastname;
+        drawText(baseX, baseY + 1, "2. Birthday: "); cout << st.getInfo().birth.day << "/" << st.getInfo().birth.month << "/" << st.getInfo().birth.year;
+        drawText(baseX, baseY + 2, "3. Gender(Male/Female): "); cout << getGender(st);
+        drawText(baseX, baseY + 3, "4. Social ID: "); cout << st.getInfo().social_id;
+        drawText(baseX, baseY + 4, "5. Exit");
         drawText(posCenter("Press a number to change/view information: "), 12, "Press a number to change/view information: ");
         cin >> id;
-        if (id == 1) { changeName(st); }
-        else if (id == 2) { changeBirthday(st); }
-        else if (id == 3) { changeGender(st); }
-        else if (id == 4) { changeSocialID(st); }
-        else if (id == 5) { viewClass(st); }
-        else if (id == 6) { viewListofCourse(st); }
+        if (id == 1){changeName(st);}
+        else if (id == 2){changeBirthday(st);}
+        else if (id == 3){changeGender(st);}
+        else if (id == 4){changeSocialID(st);}
+        //else if (id == 5){viewClass(st);}
+        //else if (id == 6){viewListofCourse(st);}
+        else break;
         system("cls");
     } while (id != 9);
     system("cls");
 }
+
+
+
+
