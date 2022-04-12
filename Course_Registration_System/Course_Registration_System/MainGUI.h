c9dfa65ee2ac5a3816fc861d5911772450671e07
Trần Kim Phát <88208222+phatalways-sleeping@users.noreply.github.com>
2022-04-12 22:30:46 +0700

    Changes in MainGUI.h

--- Course_Registration_System/Course_Registration_System/MainGUI.h
+++ Course_Registration_System/Course_Registration_System/MainGUI.h
@@ -8,55 +8,45 @@
 #include "openCourseRegistration.h"
 #include "closeCourseRegistration.h"
 #include "studentScoreboard.h"
-
+#include"createNewYear.h"
 #include "Student.h"
 #include "Staff.h"
-
-
-void drawMainGUI(Student& st, bool isStudent){
+#include"createNewCourse.h"
+#include"updateStudentResults.h"
+#include"viewScoreBoard.h"
+void drawMainGUI(Student& st, bool isStudent) {
     int baseX = 30, id;
-    while (1){
+    while (1) {
         drawText(posCenter("Course Registration System"), 1, "Course Registration System");
-        drawText(baseX, 3, "1. Profile");
-        drawText(baseX, 4, "2. Open course registration");
-        drawText(baseX, 5, "3. Close course registration");
-        drawText(baseX, 6, "4. View scoreboard");
-        drawText(baseX, 7, "5. Update a course (staff only)");
-        drawText(baseX, 8, "6. Update a student result(staff only)");
-        drawText(baseX, 9, "7. View the scoreboard of a class(staff only)");
-        drawText(baseX, 10, "8. Exit");
-
-        drawText(posCenter("Press a number: : "), 12, "Press a number: : ");
+        drawText(baseX, 3, "1. Profile"); //done
+        drawText(baseX, 4, "2. Course Registration"); 
+        drawText(baseX, 5, "3. Exit");
+        drawText(posCenter("Press a number: : "), 6, "Press a number: : ");
 
         cin >> id;
         system("cls");
 
-        if (id == 1){
+        if (id == 1) {
             drawStudentProfile(st);
         }
-        else if (id == 2){drawOpenCourse(st);}
-        else if (id == 3){drawCloseCourse(st);}
-        else if (id == 4){drawScoreboard(st);}
-        else if (id == 5){}
-        else if (id == 6){}
-        else if (id == 7){}
+        else if (id == 2) { drawOpenCourse(st); }
         else {
-            return ;
+            return;
         }
     }
 }
 
-void drawMainGUI(Staff& st, bool isStudent){
+void drawMainGUI(Staff& st, bool isStudent) {
     int baseX = 30, id;
-    while (1){
+    while (1) {
         drawText(posCenter("Course Registration System"), 1, "Course Registration System");
         drawText(baseX, 3, "1. Profile");
         drawText(baseX, 4, "2. Open course registration");
         drawText(baseX, 5, "3. Close course registration");
-        drawText(baseX, 6, "4. View scoreboard");
-        drawText(baseX, 7, "5. Update a course (staff only)");
-        drawText(baseX, 8, "6. Update a student result(staff only)");
-        drawText(baseX, 9, "7. View the scoreboard of a class(staff only)");
+        drawText(baseX, 6, "4. Create New School Year");
+        drawText(baseX, 7, "5. Create New Course");
+        drawText(baseX, 8, "6. Update a student result");
+        drawText(baseX, 9, "7. View the scoreboard of a class");
         drawText(baseX, 10, "8. Exit");
 
         drawText(posCenter("Press a number: : "), 12, "Press a number: : ");
@@ -64,17 +54,17 @@ void drawMainGUI(Staff& st, bool isStudent){
         cin >> id;
         system("cls");
 
-        if (id == 1){
-            drawStaffProfile(st);
+        if (id == 1) {
+            drawStaffProfile(st); //done
         }
-        /*else if (id == 2){drawOpenCourse(st);}
-        else if (id == 3){drawCloseCourse(st);}
-        else if (id == 4){drawScoreboard(st);}
-        */else if (id == 5){}
-        else if (id == 6){}
-        else if (id == 7){}
+        else if (id == 2) drawOpenCourse(st); //done
+        else if (id == 3) drawCloseCourse(st); //done
+        else if (id == 4) createNewYear(st); //done
+        else if (id == 5) createNewCourse(st); //done
+        else if (id == 6) updateStudentResults(st);
+        else if (id == 7) viewScoreBoard(st);
         else {
-            return ;
+            return;
         }
     }
 }
