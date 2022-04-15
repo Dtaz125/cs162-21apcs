f7af9457c66e386dd5c78aefb1b0f487697514a2
mvpanhkhoi <93597873+mvpanhkhoi@users.noreply.github.com>
2022-04-15 15:54:13 +0700

    update Course.cpp

--- Course_Registration_System/Course_Registration_System/Course.cpp
+++ Course_Registration_System/Course_Registration_System/Course.cpp
@@ -13,3 +13,16 @@ bool operator == (const Score& score_1, const Score& score_2) {
 bool operator != (const Score& score_1, const Score& score_2) {
     return score_1.id != score_2.id;
 }
+
+//ofstream& operator << (ofstream& out_file, Student& student) {
+//	return out_file;
+//}
+
+ifstream& operator >> (ifstream& in_file, Course& Course) {
+	in_file.ignore(1, '\n');
+	getline(in_file, Course.id);
+	getline(in_file, Course.name);
+	getline(in_file, Course.teacher_name);
+	getline(in_file, Course.teaching_time);
+	return in_file;
+}
