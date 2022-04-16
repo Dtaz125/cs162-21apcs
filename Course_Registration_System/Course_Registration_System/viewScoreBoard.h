ab3fcc90d1cd4ed9defeafa4a1cbf1143c3c12eb
mvpanhkhoi <93597873+mvpanhkhoi@users.noreply.github.com>
2022-04-16 14:54:48 +0700

    Update viewScoreBoard.h

--- Course_Registration_System/Course_Registration_System/viewScoreBoard.h
+++ Course_Registration_System/Course_Registration_System/viewScoreBoard.h
@@ -16,15 +16,15 @@ void changeClass(Link& link) {
 
 void findCourse(Link& link) {
     system("cls");
-    int id;
+    int id, baseY=3;
     do {
-        drawText(posCenter("VIEW COURSE SCOREBOARD"), 1, "VIEW COURSE SCOREBOARD");
-        drawText(baseX, 3, "1. School Year: "); cout << link.year;
-        drawText(baseX, 4, "2. Semester: "); cout << link.semester;
-        drawText(baseX, 5, "3. Course ID: "); cout << link.courseID;
-        drawText(baseX, 6, "4. Continue: ");
-        drawText(baseX, 7, "5. Exit: ");
-        drawText(posCenter("Press a number to change/view information: "), 8, "Press a number to change/view information: ");
+        drawText(posCenter("VIEW COURSE SCOREBOARD"), baseY+1, "VIEW COURSE SCOREBOARD");
+        drawText(baseX, baseY + 3, "1. School Year: "); cout << link.year;
+        drawText(baseX, baseY + 5, "2. Semester: "); cout << link.semester;
+        drawText(baseX, baseY + 7, "3. Course ID: "); cout << link.courseID;
+        drawText(baseX, baseY + 9, "4. Continue: ");
+        drawText(baseX, baseY + 11, "5. Exit: ");
+        drawText(posCenter("Press a number to change/view information: "), baseY + 13, "Press a number to change/view information: ");
         cin >> id;
         if (id == 1) changeYear(link);
         else if (id == 2) { changeSemester(link); }
@@ -41,12 +41,12 @@ void findClass(Link& link) {
     system("cls");
     int id;
     do {
-        drawText(posCenter("VIEW CLASS SCOREBOARD"), 1, "VIEW CLASS SCOREBOARD");
-        drawText(baseX, 3, "1. School Year: "); cout << link.year;
-        drawText(baseX, 4, "2. Class: "); cout << link.classname;
-        drawText(baseX, 5, "3. Continue: ");
-        drawText(baseX, 6, "4. Exit: ");
-        drawText(posCenter("Press a number to change/view information: "), 7, "Press a number to change/view information: ");
+        drawText(posCenter("VIEW CLASS SCOREBOARD"), baseY + 1, "VIEW CLASS SCOREBOARD");
+        drawText(baseX, baseY + 3, "1. School Year: "); cout << link.year;
+        drawText(baseX, baseY + 5, "2. Class: "); cout << link.classname;
+        drawText(baseX, baseY + 7, "3. Continue: ");
+        drawText(baseX, baseY + 9, "4. Exit: ");
+        drawText(posCenter("Press a number to change/view information: "), baseY + 11, "Press a number to change/view information: ");
         cin >> id;
         if (id == 1) changeYear(link);
         else if (id == 2) { changeClass(link); }
@@ -62,11 +62,11 @@ void viewScoreBoard(const Staff& st) {
     int id;
     Link link;
     do {
-        drawText(posCenter("VIEW SCOREBOARD"), 1, "VIEW SCOREBOARD");
-        drawText(baseX, 3, "1. Course: ");  
-        drawText(baseX, 4, "2. Class: "); 
-        drawText(baseX, 5, "3. Exit");
-        drawText(posCenter("Press a number to change/view information: "), 6, "Press a number to change/view information: ");
+        drawText(posCenter("VIEW SCOREBOARD"), baseY + 1, "VIEW SCOREBOARD");
+        drawText(baseX, baseY + 3, "1. Course: ");
+        drawText(baseX, baseY + 5, "2. Class: ");
+        drawText(baseX, baseY + 7, "3. Exit");
+        drawText(posCenter("Press a number to change/view information: "), baseY + 9, "Press a number to change/view information: ");
         cin >> id;
         if (id == 1) findCourse(link);
         else if (id == 2) { findClass(link); }
