d9a878c99dc1afeeb2af777fc45fa924fc3dfeb8
mvpanhkhoi <93597873+mvpanhkhoi@users.noreply.github.com>
2022-04-16 14:48:42 +0700

    Update closeCourseRegistration.h

--- Course_Registration_System/Course_Registration_System/closeCourseRegistration.h
+++ Course_Registration_System/Course_Registration_System/closeCourseRegistration.h
@@ -12,16 +12,16 @@ void drawCloseCourse(const Student& st) {
 }
 
 void drawCloseCourse(const Staff& st) {
-    int id;
+    int id, baseY=3;
     CourseRegistration new_c;
     do {
-        drawText(posCenter("CLOSING COURSE REGISTRATION"), 1, "CLOSING COURSE REGISTRATION");
-        drawText(baseX, baseY + 0, "1. Name: "); cout << new_c.name;
+        drawText(posCenter("CLOSING COURSE REGISTRATION"), baseY+1, "CLOSING COURSE REGISTRATION");
+        drawText(baseX, baseY + 4, "1. Name: "); cout << new_c.name;
         /*drawText(baseX, baseY + 1, "2. Start Date: "); cout << new_c.start_date.day << "/" << new_c.start_date.month << "/" << new_c.start_date.year;
         drawText(baseX, baseY + 2, "3. End Date: "); cout << new_c.end_date.day << "/" << new_c.end_date.month << "/" << new_c.end_date.year;*/
-        drawText(baseX, baseY + 1, "2. Continue to close: ");
-        drawText(baseX, baseY + 2, "3. Exit: ");
-        drawText(posCenter("Press a number to change/view information: "), baseY + 4, "Press a number to change/view information: ");
+        drawText(baseX, baseY + 6, "2. Continue to close: ");
+        drawText(baseX, baseY + 8, "3. Exit: ");
+        drawText(posCenter("Press a number to change/view information: "), baseY + 10, "Press a number to change/view information: ");
         cin >> id;
         if (id == 1) { changeName(new_c); }
         /*else if (id == 2) { changeStartDate(new_c); }
