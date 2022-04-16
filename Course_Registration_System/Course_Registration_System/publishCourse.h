9ad5a84d38303d0386997804962f3ab72eb4b2a9
mvpanhkhoi <93597873+mvpanhkhoi@users.noreply.github.com>
2022-04-16 19:20:55 +0700

    Update publishCourse.h

--- Course_Registration_System/Course_Registration_System/publishCourse.h
+++ Course_Registration_System/Course_Registration_System/publishCourse.h
@@ -3,16 +3,16 @@
 #include"other.h"
 #include"Initialize.h"
 void publishCourse(Staff& st) {
-    int baseX = 30, id;
+    int baseX = 30, id, baseY=3;
     Link link;
     while (1) {
-        drawText(posCenter("PUBLISHING COURSE"), 1, "PUBLISHING COURSE");
-        drawText(baseX, 3, "1. School Year: ");
-        drawText(baseX, 4, "2. Semester: ");
-        drawText(baseX, 5, "3. Course ID: ");
-        drawText(baseX, 6, "4. Publish: ");
-        drawText(baseX, 7, "5. Exit: ");
-        drawText(posCenter("Press a number: : "), 8, "Press a number: : ");
+        drawText(posCenter("PUBLISHING COURSE"), baseY+1, "PUBLISHING COURSE");
+        drawText(baseX, baseY + 3, "1. School Year: ");
+        drawText(baseX, baseY + 5, "2. Semester: ");
+        drawText(baseX, baseY + 7, "3. Course ID: ");
+        drawText(baseX, baseY + 9, "4. Publish: ");
+        drawText(baseX, baseY + 11, "5. Exit: ");
+        drawText(posCenter("Press a number: : "), 13, "Press a number: : ");
 
         cin >> id;
         system("cls");
