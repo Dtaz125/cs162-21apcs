defdd8f751ce596e9e62d7e38faf3dcd8b9482aa
mvpanhkhoi <93597873+mvpanhkhoi@users.noreply.github.com>
2022-04-18 21:38:17 +0700

    Update openCourseRegistration.h

--- Course_Registration_System/Course_Registration_System/openCourseRegistration.h
+++ Course_Registration_System/Course_Registration_System/openCourseRegistration.h
@@ -15,7 +15,9 @@ void drawOpenCourse(const Staff& st) {
     int id,baseY=3;
     CourseRegistration new_c;
     do {
-        drawText(posCenter("CREATE COURSE REGISTRATION"), baseY+1, "CREATE COURSE REGISTRATION");
+        drawText(posCenter("CREATE COURSE REGISTRATION"), baseY , "------------------------------");
+        drawText(posCenter("CREATE COURSE REGISTRATION"), baseY + 1, "| CREATE COURSE REGISTRATION |");
+        drawText(posCenter("CREATE COURSE REGISTRATION"), baseY + 2, "------------------------------");
         drawText(baseX, baseY + 4, "1. Name: "); cout << new_c.name;
         drawText(baseX, baseY + 6, "2. Start Date: "); cout << new_c.start_date.day << "/" << new_c.start_date.month << "/" << new_c.start_date.year;
         drawText(baseX, baseY + 8, "3. End Date: "); cout << new_c.end_date.day << "/" << new_c.end_date.month << "/" << new_c.end_date.year;
