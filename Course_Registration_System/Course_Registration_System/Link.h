581ea4a8b0b7426f5ff330329e3f3229860ac8e3
Trần Kim Phát <88208222+phatalways-sleeping@users.noreply.github.com>
2022-04-24 08:53:35 +0700

    update this

--- Course_Registration_System/Course_Registration_System/Link.h
+++ Course_Registration_System/Course_Registration_System/Link.h
@@ -3,9 +3,20 @@ struct Link {
     string year = "";
     int semester = 0;
     string courseID = "";
+    string studentID = "";
     string link = "";
     string classname = "";
+    string classname_1 = "";
 };
+
+void changeStudentID(Link& link, int baseX, int baseY, string text) {
+    string new_link;
+    drawText(baseX, baseY, text + blank);
+    drawText(baseX, baseY, text);
+    cin.ignore();
+    getline(cin, new_link);
+    link.studentID = new_link;
+}
 
 void changeYear(Link& link, int baseX, int baseY, string text) {
     string new_link;
@@ -50,4 +61,13 @@ void changeClass(Link& link, int baseX, int baseY, string text) {
     cin.ignore();
     getline(cin, new_link);
     link.classname = new_link;
+}
+
+void changeClass_1(Link& link, int baseX, int baseY, string text) {
+    string new_link;
+    drawText(baseX, baseY, text + blank);
+    drawText(baseX, baseY, text);
+    cin.ignore();
+    getline(cin, new_link);
+    link.classname_1 = new_link;
 }
